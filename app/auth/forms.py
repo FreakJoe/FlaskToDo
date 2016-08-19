1f689069a1ca24f46ad71e689e45d16da6727c2a
FreakJoe <sandmensch933@web.de>
2016-08-19 12:36:41 +0200

    Did some styling

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,14 +8,12 @@ class LoginForm(Form):
 	email = StringField('E-Mail', validators=[Required(), Length(1, 64), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	remember_me = BooleanField('Remember me')
-	submit = SubmitField('Log in')
 
 class RegistrationForm(Form):
 	email = StringField('E-Mail', validators=[Required(), Length(1, 64), Email()])
 	username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, dots and underscores.')])
 	password = PasswordField('Password', validators=[Required(), EqualTo('password_r', message='Passwords must match.')])
 	password_r = PasswordField('Confirm password', validators=[Required()])
-	submit = SubmitField('Register')
 
 	def validate_email(self, field):
 		if User.query.filter_by(email=field.data).first():
